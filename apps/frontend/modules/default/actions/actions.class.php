519aac5743fe22cc29b92b8648f0ea4eef4e83e6
Matías Alejo Roldán Zukerfeld <matiasalejoroldan@gmail.com>
2013-06-16 00:42:30 -0300

    Bugfixes

--- apps/frontend/modules/default/actions/actions.class.php
+++ apps/frontend/modules/default/actions/actions.class.php
@@ -4,5 +4,6 @@ class defaultActions extends sfActions
   public function executeIndex(sfWebRequest $request)
   {
      $this->orders = $this->getUser()->getOrders();
+     $this->publications = $this->getUser()->getPublications();
   }
 }
--- apps/frontend/modules/default/templates/indexSuccess.php
+++ apps/frontend/modules/default/templates/indexSuccess.php
@@ -1,10 +1,11 @@
 <?php if($sf_user->getMELIUser()): ?>
 
-<?php if($orders->count() > 0): ?>
-
 <div class="row-fluid">
    <div class="span12">
       <h4>Mis compras</h4>
+
+      <?php if($orders): ?>
+
       <table class="table">
          <thead>
             <tr>
@@ -16,45 +17,55 @@
             <?php foreach($orders->getRawValue() as $order): ?>
             <tr>
                <td><?php echo $order->order_items[0]->item->title; ?></td>
-               <td>Precio 1</td>
-            </tr>
-            <?php endforeach; ?>
-         </tbody>
-      </table>
+               <td><?php echo format_currency($order->order_items[0]->unit_price, $order->order_items[0]->currency_id); ?></td>
+               <td><td>
+                  </tr>
+                  <?php endforeach; ?>
+               </tbody>
+            </table>
 
-   </div>
-</div>
+            <?php else: ?>
 
-<?php endif; ?>
+            <div class="alert alert-info">No se encontraron compras realizadas</div>
 
-<div class="row-fluid">
-   <div class="span12">
-      <h4>Mis publicaciones</h4>
-      <table class="table">
-         <thead>
-            <tr>
-               <th>Producto</th>
-               <th>Precio</th>
-            </tr>
-         </thead>
-         <tbody>
-            <tr>
-               <td>Producto 1</td>
-               <td>Precio 1</td>
-            </tr>
-            <tr>
-               <td>Producto 1</td>
-               <td>Precio 1</td>
-            </tr>
-            <tr>
-               <td>Producto 1</td>
-               <td>Precio 1</td>
-            </tr>
-         </tbody>
-      </table>
-   </div>
-</div>
+            <?php endif; ?>
 
-<?php else: ?>
-<a class="btn btn-success btn-xlarge" href="<?php echo $sf_user->getMELI()->getLoginUrl(); ?>"><i class="icon-user"></i> Login con Mercado Libre</a>
-<?php endif; ?>
+         </div>
+      </div>
+
+
+      <div class="row-fluid">
+         <div class="span12">
+            <h4>Mis publicaciones</h4>
+
+            <?php if($publications): ?>
+
+            <table class="table">
+               <thead>
+                  <tr>
+                     <th>Producto</th>
+                     <th>Precio</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  <?php foreach($publications as $publication): ?>
+                  <tr>
+                     <td>Producto 1</td>
+                     <td>Precio 1</td>
+                  </tr>
+                  <?php endforeach; ?>
+               </tbody>
+            </table>
+
+            <?php else: ?>
+
+            <div class="alert alert-info">No se encontraron publicaciones</div>
+
+            <?php endif; ?>
+
+         </div>
+      </div>
+
+      <?php else: ?>
+      <a class="btn btn-success btn-xlarge" href="<?php echo $sf_user->getMELI()->getLoginUrl(); ?>"><i class="icon-user"></i> Login con Mercado Libre</a>
+      <?php endif; ?>
